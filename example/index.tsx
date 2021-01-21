8e881129c4d3a55d273f4bdacca7a19a54a7e8b2
qiuwei <qiuwei@bizseer.com>
2021-01-21 15:20:56 +0800

    test: 添加示例

---
+++ example/index.tsx
@@ -0,0 +1,32 @@
+import * as React from 'react'
+import * as ReactDOM from 'react-dom'
+
+import 'antd/dist/antd.css'
+
+import { FieldValueType, QueryInput } from '../.'
+import { DistinctField } from '../dist/hooks/useSyntaxSuggestions'
+
+const fields: DistinctField[] = [{
+  name: 'application',
+  valueType: FieldValueType.str,
+  status: false
+}, {
+  name: 'service',
+  valueType: FieldValueType.str,
+  status: false
+}, {
+  name: 'host',
+  valueType: FieldValueType.str,
+  status: false
+}, {
+  name: 'level',
+  valueType: FieldValueType.num,
+  status: false
+}]
+
+ReactDOM.render(
+  <div style={ { padding: 24 } }>
+    <QueryInput fieldOptionItems={fields} />
+  </div>,
+  document.getElementById('root'),
+)
