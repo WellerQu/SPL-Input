ee72745c87ba466ad58719aa7c751f605ae53d6b
zhouwenqing <zhouwenqing@bizseer.com>
2021-04-29 15:17:54 +0800

    feat:增加字段值tag

--- typings/spl.d.ts
+++ typings/spl.d.ts
@@ -1,7 +1,7 @@
 
 interface SuggestionItem {
   label: string
-  tag: '关键词' | '函数' | '字段' | '通用' | '符号' | '算子' | '逻辑'
+  tag: '关键词' | '函数' | '字段' | '通用' | '符号' | '算子' | '逻辑' | '字段值'
   mapping: string
   code: string
   syntax?: string
